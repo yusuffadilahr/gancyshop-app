96a98d18d3a2daec05fd61da4abba2206d5cdf89
Yusuf Fadilah Rukmana <yusuffadilah58@gmail.com>
2025-07-13 10:28:52 +0700

    chore: refactore code for modular and reusable

---
+++ src/app/(landingmenu)/product/_clientside/hooks/use-helper-product.ts
@@ -0,0 +1,63 @@
+import { IValueOnChange } from "@/app/(landingmenu)/product/_clientside/types";
+import * as React from 'react'
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+
+export const useHelperProductPage = () => {
+    const params = useSearchParams()
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const [page, setPage] = React.useState<number>(Number(params?.get('page')) || 1)
+    const limit = 8
+    const [datePicker, setDatePicker] = React.useState<Date | undefined>(undefined)
+    const [valueOnChange, setValueOnChange] = React.useState<IValueOnChange>({
+        searchProduct: '', kategoriId: params?.get('category-id') || '',
+        minPrice: '', maxPrice: '',
+        minWeight: '', maxWeight: '',
+        stock: ''
+    })
+
+    const debounceMinPrice = useDebouncedCallback(val => {
+        setValueOnChange(prev => ({ ...prev, minPrice: val }))
+    }, 800)
+
+    const debounceMaxPrice = useDebouncedCallback(val => {
+        setValueOnChange(prev => ({ ...prev, maxPrice: val }))
+    }, 800)
+
+    const debounceMinWeight = useDebouncedCallback(val => {
+        setValueOnChange(prev => ({ ...prev, minWeight: val }))
+    }, 800)
+
+    const debounceMaxWeight = useDebouncedCallback(val => {
+        setValueOnChange(prev => ({ ...prev, maxWeight: val }))
+    }, 800)
+
+    const handleResetFilter = () => {
+        setPage(1)
+        setValueOnChange({
+            kategoriId: '',
+            maxPrice: '',
+            maxWeight: '',
+            minPrice: '',
+            minWeight: '',
+            searchProduct: '',
+            stock: ''
+        })
+
+        setDatePicker(undefined)
+    }
+
+    return {
+        page, setPage, limit,
+        datePicker, setDatePicker,
+        valueOnChange, setValueOnChange,
+        debounceMinWeight,
+        debounceMinPrice,
+        debounceMaxWeight,
+        debounceMaxPrice,
+        handleResetFilter,
+        router, pathname, params,
+    }
+}
