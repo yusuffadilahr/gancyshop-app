606a2a1b73614c907926c0cb7d7913912faf2c22
yusuffadilahr <yusuffadilah58@gmail.com>
2025-09-07 21:15:49 +0700

    fix: fixing landing page

--- src/app/_clients/components/bodyLandingPage.tsx
+++ src/app/_clients/components/bodyLandingPage.tsx
@@ -5,16 +5,30 @@ import SectionCarousell from "@/app/_clients/components/sectionCarousell";
 import SectionCallToAction from "./sectionCallToAction";
 import SectionProduct from "./sectionProduct";
 import { dummyData } from "@/app/_servers/utils/dummyData";
+import dynamic from "next/dynamic";
+import { Spinner } from "@/components/ui/spinner";
+
+const DynamicFirstVisitModal = dynamic(() => import("./firstVisitModal"), {
+  loading: () => <Spinner />,
+});
 
 export default function BodyLanding() {
+  const [isOpenDialog, setIsOpenDialog] = React.useState<boolean>(false);
+
   React.useEffect(() => {
     console.log("Welcome, Diddy!");
+    const checkCookie = sessionStorage.getItem("_inf");
+    if (!checkCookie) setIsOpenDialog(true);
   }, []);
 
   return (
     <div className="pb-20 md:px-4 min-h-screen h-fit space-y-4">
-      <SectionCarousell />
+      <DynamicFirstVisitModal
+        isOpenDialog={isOpenDialog}
+        setIsOpenDialog={setIsOpenDialog}
+      />
 
+      <SectionCarousell />
       <SectionProduct dataProduct={dummyData} />
       <SectionCardPlatform />
       <SectionCallToAction />
---
+++ src/app/_clients/components/firstVisitModal.tsx
@@ -0,0 +1,63 @@
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { encryptCrypto } from "../utils/cryptoJs";
+
+interface IFirstVisitModal {
+  isOpenDialog: boolean;
+  setIsOpenDialog: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function FirstVisitModal({
+  isOpenDialog,
+  setIsOpenDialog,
+}: IFirstVisitModal) {
+  return (
+    <Dialog
+      open={isOpenDialog}
+      onOpenChange={(open) => {
+        const checked = sessionStorage.getItem("_inf");
+        const dataSessionStrg = encryptCrypto({
+          val: "done",
+          key: process.env.NEXT_PUBLIC_SECRET_KEY || "",
+        });
+
+        if (!checked)
+          sessionStorage.setItem("_inf", dataSessionStrg.toString());
+
+        setIsOpenDialog(open);
+      }}
+    >
+      <DialogTitle className="sr-only">x</DialogTitle>
+      <DialogContent className="p-0 shadow-none border-none bg-transparent">
+        <Card className="relative w-full shadow-none border-none mx-auto p-8 bg-gradient-to-r from-red-500 to-orange-400 rounded-3xl overflow-hidden">
+          <div className="absolute -top-[90%] -right-20 w-[1000px] h-[1000px] bg-white/20 rounded-full"></div>
+          <div className="absolute -top-[70%] -right-10 w-[800px] h-[800px] bg-white/40 rounded-full"></div>
+          <div className="absolute -top-[50%] -right-20 w-[600px] h-[600px] bg-white/50 rounded-full"></div>
+
+          <div className="relative z-10 inset-0 p-2 md:p-10">
+            <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
+              Gancy Motor Parts
+            </h1>
+            <p className="text-white/90 text-lg mb-8 max-w-md">
+              Solusi terpercaya untuk semua kebutuhan{" "}
+              <span className="font-extrabold">body kit & sparepart motor</span>{" "}
+              berkualitas tinggi.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-4">
+              <Link href="/product" prefetch={false}>
+                <Button size="lg" className="rounded-xl">
+                  <span>Pesan Sekarang</span>
+                  <ArrowRight />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Card>
+      </DialogContent>
+    </Dialog>
+  );
+}
