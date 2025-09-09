e057e6775dcbb41c6539d552c0b9cce0b49d220e
yusuffadilahr <yusuffadilah58@gmail.com>
2025-09-09 20:43:39 +0700

    fix: fixing for deploy

--- src/app/(landingmenu)/product/_servers/services/index.ts
+++ src/app/(landingmenu)/product/_servers/services/index.ts
@@ -1,46 +1,50 @@
-'use server'
+"use server";
 
-import { baseUrlApi } from "@/app/_clients/utils/axiosInstance"
+import { baseUrlApi } from "@/app/_clients/utils/axiosInstance";
 // import { cookies } from "next/headers"
 
 export const getAllProductPublic = async ({
-    search = '',
-    page = 1,
-    limit = 10,
-    category = '',
-    tanggal = '',
-    minPrice = '',
-    maxPrice = '',
-    minWeight = '',
-    maxWeight = '',
-    stock = '',
+  search = "",
+  page = 1,
+  limit = 10,
+  category = "",
+  tanggal = "",
+  minPrice = "",
+  maxPrice = "",
+  minWeight = "",
+  maxWeight = "",
+  stock = "",
 }: {
-    search: string
-    page: number,
-    limit: number,
-    category?: string,
-    tanggal?: string
-    minPrice?: string
-    maxPrice?: string
-    minWeight?: string
-    maxWeight?: string
-    stock?: string
+  search: string;
+  page: number;
+  limit: number;
+  category?: string;
+  tanggal?: string;
+  minPrice?: string;
+  maxPrice?: string;
+  minWeight?: string;
+  maxWeight?: string;
+  stock?: string;
 }) => {
-    try {
-        const url = `${baseUrlApi}/product/all-product`
+  try {
+    const url = `${baseUrlApi}/product/all-product`;
 
-        const res = await fetch(`${url}?search=${search}&page=${page}&limit=${limit}&category=${category}&tanggalDibuat=${tanggal}&minPrice=${minPrice}&maxPrice=${maxPrice}&minWeight=${minWeight}&maxWeight=${maxWeight}&stock=${stock}`, {
-            method: 'GET',
-            cache: 'no-store',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        const result = await res.json()
-        if (!res.ok) throw new Error('Gagal mendapatkan data')
+    const res = await fetch(
+      `${url}?search=${search}&page=${page}&limit=${limit}&category=${category}&tanggalDibuat=${tanggal}&minPrice=${minPrice}&maxPrice=${maxPrice}&minWeight=${minWeight}&maxWeight=${maxWeight}&stock=${stock}`,
+      {
+        method: "GET",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const result = await res.json();
+    console.log(result, "<<");
+    if (!res.ok) throw result
 
-        return result
-    } catch (error) {
-        if (error) return []
-    }
-}
+    return result;
+  } catch (error) {
+    if (error) return [];
+  }
+};
